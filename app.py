5ca1271ae3766f9d8fdb3fab136c75c6cccf5eac
yceballost <yceballost@gmail.com>
2023-06-12 18:34:27 +0200

    update

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 import os
 import random
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 def generate_json_from_image_name(image_name):
     # Separar el nombre de la imagen en sus componentes
@@ -11,15 +14,23 @@ def generate_json_from_image_name(image_name):
     variant = components[1]
     version = components[2].split('.')[0]
 
+    # Crear la URL absoluta de la imagen
+    image_url = f"{request.base_url.rsplit('/', 1)[0]}/{image_name}"
+
     # Crear el diccionario JSON
     data = {
-        "image": f"{image_name}",
+        "image": image_url,
         "name": name.capitalize(),
         "variant": variant,
         "version": version
     }
 
     return data
+
+
+def generate_image_path_from_image_name(image_name):
+    return f"logos/{image_name}"
+
 
 @app.route("/")
 def generate_json():
@@ -55,6 +66,30 @@ def get_random_json():
 
     # Devolver el elemento aleatorio como respuesta JSON
     return jsonify(json_data)
+
+@app.route("/<name>")
+def get_logo_variants(name):
+    # Obtener la lista de archivos en la carpeta "img"
+    folder_path = "img"
+    image_files = os.listdir(folder_path)
+
+    # Filtrar los resultados según el nombre del logo
+    variants = []
+    for image_file in image_files:
+        logo_name = image_file.split('-')[0]
+        if logo_name.lower() == name.lower():
+            variant = generate_json_from_image_name(image_file)
+            variants.append(variant)
+
+    if variants:
+        if len(variants) == 1:
+            return jsonify(variants[0])
+        else:
+            return jsonify(variants)
+    else:
+        return f"No se encontró ningún logo con el nombre '{name}'", 404
+
+
 
 if __name__ == "__main__":
     app.run(debug=False)
