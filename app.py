2a92fa0432448ef9834b88e21c0b7e792cab569c
yceballost <yceballost@gmail.com>
2023-06-12 16:35:56 +0200

    update

--- app.py
+++ app.py
@@ -1,59 +1,52 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+import os
 import random
 
 app = Flask(__name__)
 
-# Dummy database of brand logos with variants
-logos = [
-    {
-        "name": "Nike",
-        "variants": {
-            "normal": "logos/nike.png",
-            "bleeded": "logos/nike.png",
-            "squared": "logos/nike.png"
-        }
-    },
-    {
-        "name": "Mailchimp",
-        "variants": {
-            "normal": "logos/mailchimp.png",
-            "bleeded": "logos/mailchimp.png",
-            "squared": "logos/mailchimp.png"
-        }
+def generate_json_from_image_name(image_name):
+    # Separar el nombre de la imagen en sus componentes
+    components = image_name.split('-')
+    name = components[0]
+    variant = components[1]
+    version = components[2].split('.')[0]
+
+    # Crear el diccionario JSON
+    data = {
+        "image": f"logos/{image_name}",
+        "name": name.capitalize(),
+        "variant": variant,
+        "version": version
     }
-]
 
-@app.route('/logo/random', methods=['GET'])
+    return data
+
+@app.route("/")
+def generate_json():
+    # Obtener la lista de archivos en la carpeta "img"
+    folder_path = "src"
+    image_files = os.listdir(folder_path)
+
+    # Generar el JSON para cada imagen encontrada
+    json_data = []
+    for image_file in image_files:
+        json_data.append(generate_json_from_image_name(image_file))
+
+    # Devolver el JSON como respuesta
+    return jsonify(records=json_data)
+
+@app.route("/random")
 def get_random_logo():
-    logo = random.choice(logos)
-    variant = random.choice(list(logo["variants"].keys()))
-    image = logo["variants"][variant]
-    return jsonify({"name": logo["name"], "variant": variant, "image": image})
+    # Obtener la lista de archivos en la carpeta "img"
+    folder_path = "src"
+    image_files = os.listdir(folder_path)
 
-@app.route('/logo/<name>', methods=['GET'])
-def get_logo_by_name(name):
-    variant = request.args.get('variant')
-    for logo in logos:
-        if logo["name"].lower() == name.lower():
-            if variant and variant in logo["variants"]:
-                image = logo["variants"][variant]
-                return jsonify({"name": logo["name"], "variant": variant, "image": image})
-            else:
-                variant = random.choice(list(logo["variants"].keys()))
-                image = logo["variants"][variant]
-                return jsonify({"name": logo["name"], "variant": variant, "image": image})
-    return jsonify({"error": "Logo not found"})
+    # Seleccionar un logotipo aleatorio
+    random_image_file = random.choice(image_files)
+    random_logo = generate_json_from_image_name(random_image_file)
 
-@app.route('/logo/random/<variant>', methods=['GET'])
-def get_random_logo_with_variant(variant):
-    logo = random.choice(logos)
-    if variant and variant in logo["variants"]:
-        image = logo["variants"][variant]
-        return jsonify({"name": logo["name"], "variant": variant, "image": image})
-    else:
-        variant = random.choice(list(logo["variants"].keys()))
-        image = logo["variants"][variant]
-        return jsonify({"name": logo["name"], "variant": variant, "image": image})
+    # Devolver el logotipo aleatorio como respuesta JSON
+    return jsonify(random_logo)
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=False)
