745e3d611f1cb1aaba752379bd9e6490d37df308
yceballost <yceballost@gmail.com>
2023-06-13 21:14:06 +0200

    added priority for color logos

--- src/app.py
+++ src/app.py
@@ -10,6 +10,11 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 def generate_json_from_image_name(image_name):
     # Separar el nombre de la imagen en sus componentes
     components = image_name.split('-')
+
+    if len(components) < 3:
+        print(f"Not valid: {image_name}")
+        return None
+
     name = components[0]
     variant = components[1]
     version = components[2].split('.')[0]
@@ -27,8 +32,9 @@ def generate_json_from_image_name(image_name):
 
     return data
 
-def generate_image_path_from_image_name(image_name):
-    return f"logos/{image_name}"
+
+# def generate_image_path_from_image_name(image_name):
+#     return f"logos/{image_name}"
 
 @app.route("/all")
 def generate_json():
@@ -78,7 +84,7 @@ def get_random_json():
         return redirect(image_url)
 
     else:
-        return f"No se encontró ningún logo con los parámetros especificados", 404
+        return f"No logo found with the specified parameters", 404
 
 
 @app.route("/<name>")
@@ -102,10 +108,13 @@ def get_logo_variants(name):
             filtered_images.append(image_data)
 
     if filtered_images:
-        image_name = filtered_images[0]['image'].split('/')[-1]
+        # Ordenar las imágenes por versión, de forma que la versión en color aparezca antes
+        sorted_images = sorted(filtered_images, key=lambda x: x['version'], reverse=True)
+
+        image_name = sorted_images[0]['image'].split('/')[-1]
         return send_from_directory(app.static_folder, f"images/{image_name}")
     else:
-        return f"No se encontró ningún logo con los parámetros especificados", 404
+        return f"No logo found with the specified parameters", 404
 
 
 if __name__ == "__main__":
