d45f4ce5e9c6e018fc799bc3aef7634df6819591
Ignacio Ceballos (Yayo) <yceballost@gmail.com>
2025-01-01 16:33:19 +0100

    favicon path

--- api/random_favicon.js
+++ api/random_favicon.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     const logoDir = path.resolve("./static/logos");
     const files = fs.readdirSync(logoDir);
 
-    // Filtrar logos que contengan "glyph" y "color"
+    // Filtrar logos que contienen "glyph" y "color"
     const logos = files.filter(
       (file) =>
         file.includes("glyph") &&
@@ -20,13 +20,12 @@ export default async function handler(req, res) {
       return;
     }
 
-    // Seleccionar un logo aleatorio
     const selectedLogo = logos[Math.floor(Math.random() * logos.length)];
     const filePath = path.join(logoDir, selectedLogo);
 
     // Configurar encabezados para servir el archivo
     res.setHeader("Content-Type", "image/svg+xml");
-    res.setHeader("Cache-Control", "public, max-age=3600"); // Cachea por 1 hora
+    res.setHeader("Cache-Control", "public, max-age=3600"); // Cache por 1 hora
     res.status(200).send(fs.readFileSync(filePath));
   } catch (error) {
     console.error("Error serving favicon:", error);
